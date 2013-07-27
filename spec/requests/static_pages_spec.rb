3c4deeb1b2ddf0ecf9f6c5139af9a6f23da05237
Quyen Nguyen <ma65p2004@gmail.com>
2013-07-27 08:05:55 -0500

    Add contact, Guard, Spork, and full_title Spec to application helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,8 +11,15 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
+
+
+    it "should not have a custom Title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
+    end
+
   end
 
   describe "Help page" do
@@ -40,4 +47,20 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+
+  describe "Contact Page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+
 end
